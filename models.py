231ac4a6239104b27c49100af53e262aaed2c52a
stevie-nomad <56633995+stevie-nomad@users.noreply.github.com>
2021-12-16 12:44:07 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Questions(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub.date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.integerField(default=0)    
