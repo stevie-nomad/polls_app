8f02dd235199e2366db6ec163cffd4ea8e7561d6
stevie-nomad <56633995+stevie-nomad@users.noreply.github.com>
2022-01-06 11:00:56 +0300

    Add files via upload
    Added HttpResponse to view.py

--- views.py
+++ views.py
@@ -1,9 +1,8 @@
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+from django.http import HttpResponse
 
-def results(request, question_id):
-    response = "You're are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+from .models import question_id
 
-def vote(request, question_id):
-    return HttpResponse("YOu're voting on question %s." % question_id)    
+def index(request):
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
