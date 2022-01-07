23c13c6eda5fe45c6aaf7fb433332bd047a9b30f
stevie-nomad <56633995+stevie-nomad@users.noreply.github.com>
2022-01-07 11:31:24 +0300

    Add files via upload
    The get_object_or_404 function takes a Django model as it's first argument. It raises the Http404 if the object does not exist.

--- views.py
+++ views.py
@@ -1,11 +1,7 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
-def detail(request, request, question_id):
-  try:
-      question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-      raise Http404("Question does not exist")
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/detail.html', {'question': question})        
