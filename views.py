ee737193d295036f01a08d5478b43ec7bab3dfd5
stevie-nomad <56633995+stevie-nomad@users.noreply.github.com>
2022-01-07 12:16:46 +0300

    Add files via upload
    Added a working vote( ) function which redirects to the result page

--- views.py
+++ views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Question
-
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/detail.html', {'question': question})        
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
