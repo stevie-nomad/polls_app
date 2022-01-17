1be81324ceb5937b307d42e91325b0b9672b421e
stevie-nomad <56633995+stevie-nomad@users.noreply.github.com>
2022-01-17 12:36:40 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Question(models.Model):
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
